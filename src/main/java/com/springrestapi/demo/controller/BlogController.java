25226702d7ec178b1bec55c9b37b912e09495ec8
arionitmisini <arionitmisini@gmail.com>
2018-06-21 19:56:24 +0200

    Features: CRUD Operation completed

--- src/main/java/com/springrestapi/demo/controller/BlogController.java
+++ src/main/java/com/springrestapi/demo/controller/BlogController.java
@@ -16,56 +16,36 @@ public class BlogController {
     private BlogRepository blogRepository;
 
     @GetMapping("/blog")
-    public List<Blog> index(){
+    public List<Blog> index() {
         return blogRepository.findAll();
     }
 
     @GetMapping("/blog/{id}")
-    public Optional<Blog> show(@PathVariable int id){
+    public Optional<Blog> show(@PathVariable int id) {
         return blogRepository.findById(id);
-    }
-
-    @PostMapping("/blog/search")
-    public List<Blog> search(@RequestBody Map<String, String> body){
-        String searchTerm = body.get("text");
-        return blogRepository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
-    }
-
-    /*@GetMapping("/blog")
-    public List<Blog> index() {
-        return blogMockedData.fetchBlogs();
-    }
-
-    @GetMapping("/blog/{id}")
-    public Blog show(@PathVariable int id) {
-        return blogMockedData.getBlogById(id);
     }
 
     @PostMapping("/blog/search")
     public List<Blog> search(@RequestBody Map<String, String> body) {
         String searchTerm = body.get("text");
-        return blogMockedData.searchBlogs(searchTerm);
+        return blogRepository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
     }
 
     @PostMapping("/blog")
     public Blog create(@RequestBody Map<String, String> body) {
-
-        int id = Integer.parseInt(body.get("id"));
         String title = body.get("title");
         String content = body.get("content");
-
-        return blogMockedData.createBlog(id, title, content);
-
+        System.out.println(title);
+        return blogRepository.save(new Blog(title, content));
     }
 
-    @PutMapping("/blog/{id}")
-    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body) {
+
+
+    @DeleteMapping("blog/{id}")
+    public boolean delete(@PathVariable String id){
         int blogId = Integer.parseInt(id);
-        String title = body.get("title");
-        String content  = body.get("content");
-
-        return blogMockedData.updateBlog(blogId, title, content);
-
+        blogRepository.deleteById(blogId);
+        return true;
     }
-*/
+
 }
--- src/main/java/com/springrestapi/demo/models/Blog.java
+++ src/main/java/com/springrestapi/demo/models/Blog.java
@@ -3,48 +3,49 @@ package com.springrestapi.demo.models;
 import javax.persistence.*;
 
 @Entity
-public class Blog implements Comparable<Blog>{
+@Table(name = "blog")
+public class Blog implements Comparable<Blog> {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private int id;
-    @Column(name="title")
+    @Column(name = "title")
     private String title;
-    @Column(name="content")
+    @Column(name = "content")
     private String content;
 
-    public Blog(){
+    public Blog() {
 
     }
 
-    public Blog(int id, String title, String content){
+    public Blog(String title, String content) {
 
-        this.id = id;
+
         this.title = title;
         this.content = content;
 
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getContent(){
+    public String getContent() {
         return content;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setContent(String content){
+    public void setContent(String content) {
         this.content = content;
     }
 
